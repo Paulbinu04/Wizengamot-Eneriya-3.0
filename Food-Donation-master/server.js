698901cec7cbe3dd1ed3f1b812910fe39c8545aa
Alan Antony <voa.alan@gmail.com>
2025-02-15 09:57:32 +0530

    SavRY

--- Food-Donation-master/server.js
+++ Food-Donation-master/server.js
@@ -60,33 +60,42 @@ app.post("/filter", (req, res) => {
 });
 
 // Endpoint to predict food wastage amount
-app.post("/predict", (req, res) => {
-  const {
-    type_of_food,
-    number_of_guests,
-    event_type,
-    quantity_of_food,
-    storage_conditions,
-    purchase_history,
-    seasonality,
-    preparation_method,
-    geographical_location,
-    pricing,
-  } = req.body;
+// app.post("/predict", (req, res) => {
+//   const {
+//     type_of_food,
+//     number_of_guests,
+//     event_type,
+//     quantity_of_food,
+//     storage_conditions,
+//     purchase_history,
+//     seasonality,
+//     preparation_method,
+//     geographical_location,
+//     pricing,
+//   } = req.body;
 
-  const command = `python predict.py "${type_of_food}" ${number_of_guests} "${event_type}" ${quantity_of_food} "${storage_conditions}" "${purchase_history}" "${seasonality}" "${preparation_method}" "${geographical_location}" "${pricing}"`;
+//   const command = `python predict.py "${type_of_food}" ${number_of_guests} "${event_type}" ${quantity_of_food} "${storage_conditions}" "${purchase_history}" "${seasonality}" "${preparation_method}" "${geographical_location}" "${pricing}"`;
 
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return res.status(500).send("Error predicting food wastage amount");
-    }
-    res.send({ prediction: parseFloat(stdout) });
-  });
-});
+//   exec(command, (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`exec error: ${error}`);
+//       return res.status(500).send("Error predicting food wastage amount");
+//     }
+//     res.send({ prediction: parseFloat(stdout) });
+//   });
+// });
 app.get("/", function (req, res) {
   res.render("home", { name: null });
 });
+
+app.get("/food-calculator", function (req, res) {
+  res.render("food_calculator");
+});
+
+app.get("/recycle", function (req, res) {
+  res.render("recycle");
+});
+
 
 app.get("/donation", function (req, res) {
   if(user=="donor"){
